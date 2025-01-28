a5b05d4e66e356d9507b23924b9377ab82a8933c
Waaiz <waaizkhann@gmail.com>
2025-01-27 22:26:37 -0600

    Added new packages (koa-router, koa-bodyparser) and used them

--- src/server.js
+++ src/server.js
@@ -3,30 +3,45 @@ const config = require("config");
 const Koa = require('koa');
 const fs = require('fs');
 const https = require('https');
+const bodyParser = require('koa-bodyparser'); 
+const Router = require('koa-router');
 
 
 log4js.configure(config.get("log4js"));
-  
-const logger = log4js.getLogger("oops");
+const logger = log4js.getLogger("Configured the logger.");
   
 const portNum = config.get("secure-port"); // getting port from config file
 logger.info(`The port number is: ${portNum}`);
 
+
 //console.log("hello");
 logger.error("Cheese is too ripe!");
 
-//console.log("byebye");
-logger.debug("Got cheese.");
-
 const app = new Koa();
+const genreRouter = new Router(
+  {
+    prefix: '/genre'
+  }
+);
+
+genreRouter.get('/', async (ctx, next) => {
+  ctx.response.body = "Not yet implemented!";
+  ctx.status = 501;
+});
+
+genreRouter.get('/:genre_id', async (ctx, next) => {
+  ctx.response.body = "Not yet implemented!";
+  ctx.status = 501;
+})
+
+
 
 const options = {
     key: fs.readFileSync('certs/private-key.pem'),
     cert: fs.readFileSync('certs/certificate.pem'),
   };
-  
 
-
+app.use(bodyParser());
 
 // response
 app.use(async (ctx, next) => {
@@ -40,12 +55,14 @@ app.use(async (ctx, next) => {
     console.log(`It took ${elapsed_time} ms to complete the request.`)
   });
 
-app.use(async (ctx) => {
-    const content = fs.readFileSync('src/public/index.html');
-    ctx.headers['content-type'] = 'text/html';
-    ctx.status = 201;
-    ctx.body = content.toString();
-});
+ app.use(genreRouter.routes());
+
+// app.use(async (ctx) => {
+//     const content = fs.readFileSync('src/public/index.html');
+//     ctx.headers['content-type'] = 'text/html';
+//     ctx.status = 201;
+//     ctx.body = content.toString();
+// });
 
 try {
     const server = https.createServer(options, app.callback());
